32c893513d3eb52fafd45732def5645625320473
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2020-09-30 12:25:02 +0500

    Added sample classes for url to jpg & png

---
+++ app/services/pdf_co/render_url.rb
@@ -0,0 +1,18 @@
+module PdfCo
+  class RenderUrl
+
+    PDF_URL = 'http://www.africau.edu/images/default/sample.pdf'
+
+    class << self
+      def url_to_png
+        response = PdfCo::Request.where('pdf/convert/to/png', { url: PDF_URL}, :post)
+        response['urls'] || response['error']
+      end
+
+      def url_to_jpg
+        response = PdfCo::Request.where('pdf/convert/to/jpg', { url: PDF_URL }, :post)
+        response['urls'] || response['error']
+      end
+    end
+  end
+end
--- app/services/pdf_co/request.rb
+++ app/services/pdf_co/request.rb
@@ -1,8 +1,13 @@
 module PdfCo
   class Request
     class << self
-      def where(resource_path, query = {}, options = {})
-        response, status = get_json(resource_path, query)
+
+      SUPPORTED_METHODS = [:get, :post, :put, :delete]
+
+      def where(resource_path, query = {}, method = :get)
+        return custom_error('invalid request method',  400) unless SUPPORTED_METHODS.include?(method)
+
+        response, status = get_json(resource_path, query, method)
         status == 200 ? response : errors(response)
       end
 
@@ -11,10 +16,15 @@ module PdfCo
         response.merge(error)
       end
 
-      def get_json(root_path, query = {})
+      def custom_error(message,  code)
+        { 'error' => { status: code, message: message } }
+      end
+
+      def get_json(root_path, query = {}, method)
+        byebug
         query_string = query.map{|k,v| "#{k}=#{v}"}.join("&")
         path = query.empty?? root_path : "#{root_path}?#{query_string}"
-        response = api.post(path)
+        response = api.send(method, path)
         [JSON.parse(response.body), response.status]
       end
 
