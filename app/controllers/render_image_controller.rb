126841f634f2b384ba037caaab4dc3295a034474
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2020-10-01 12:43:51 +0500

    Added render image end point

---
+++ app/controllers/render_image_controller.rb
@@ -0,0 +1,36 @@
+class RenderImageController < ApplicationController
+
+  before_action :validate_params
+  before_action :validate_end_point
+
+  ALLOWED_ENDPOINTS = ['png', 'jpg']
+
+  def show
+    response = PdfCo::Request.where(:post, "pdf/convert/to/#{params[:end_point]}", render_params)
+    return render_response(response)
+  end
+
+  private
+
+  def validate_params
+    return render json: { status: 400, error: true, message: 'url field is missing' } if params[:url].blank?
+  end
+
+  def validate_end_point
+    return if ALLOWED_ENDPOINTS.include?(params[:end_point])
+    return render json: { status: 400, error: true, message: 'invalid end point' }
+  end
+
+  def render_params
+    params.permit(:url)
+  end
+
+  def render_response(response)
+    if response['error'].present?
+      return render json: { status: response['status'], errorCode: response['errorCode'], message: response['message'] }
+    else
+      # we can convert the url into base64 as well if required.
+      return render json: { urls: response['urls'], pageCount: response['pageCount'], name: response['name'] }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/api/convert/*end_point", to: "convert#show"
+  get "/api/render_image/*end_point", to: "render_image#show"
 end
